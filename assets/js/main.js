680031708d5f6714a0d5d7ba77eb7df5572a5835
dlwlrma <tpgks2020@naver.com>
2021-02-08 15:22:07 +0900

    33

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,212 +1,250 @@
-"use strict";
+/*
+	Landed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-//Rage Template
-//Designerd by: http://bootstrapthemes.co
+(function($) {
 
-jQuery(document).ready(function ($) {
+	var	$window = $(window),
+		$body = $('body');
 
-//for Preloader
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
 
-    $(window).load(function () {
-        $("#loading").fadeOut(500);
-    });
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
+	// Touch mode.
+		if (browser.mobile)
+			$body.addClass('is-touch');
 
-    /*---------------------------------------------*
-     * Mobile menu
-     ---------------------------------------------*/
-    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: (target.offset().top - 80)
-                }, 1000);
-                if ($('.navbar-toggle').css('display') != 'none') {
-                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
-                }
-                return false;
-            }
-        }
-    });
+	// Scrolly links.
+		$('.scrolly').scrolly({
+			speed: 2000
+		});
 
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			alignment: 'right',
+			hideDelay: 350
+		});
 
+	// Nav.
 
-    /*---------------------------------------------*
-     * WOW
-     ---------------------------------------------*/
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
 
-    var wow = new WOW({
-        mobile: false // trigger animations on mobile devices (default is true)
-    });
-    wow.init();
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
-// magnificPopup
+	// Parallax.
+	// Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
+		if (browser.name == 'ie'
+		||	browser.mobile) {
 
-    $('.popup-img').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
+			$.fn._parallax = function() {
 
-    $('.video-link').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        iframe: {
-  markup: '<div class="mfp-iframe-scaler">'+
-            '<div class="mfp-close"></div>'+
-            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+				return $(this);
 
-  patterns: {
-    youtube: {
-      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+			};
 
-      id: 'v=', // String that splits URL in a two parts, second part should be %id%
-      // Or null - full URL will be returned
-      // Or a function that should return %id%, for example:
-      // id: function(url) { return 'parsed id'; }
+		}
+		else {
 
-      src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
-    }
-    // you may add here more sources
+			$.fn._parallax = function() {
 
-  },
+				$(this).each(function() {
 
-  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-}
- 
-    });
+					var $this = $(this),
+						on, off;
 
+					on = function() {
 
+						$this
+							.css('background-position', 'center 0px');
 
-// slick slider active Home Page Tow
-    $(".testimonial_slid").slick({
-        dots: false,
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: "<i class='fa fa-angle-left nextprevleft'></i>",
-        nextArrow: "<i class='fa fa-angle-right nextprevright'></i>",
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+						$window
+							.on('scroll._parallax', function() {
 
+								var pos = parseInt($window.scrollTop()) - parseInt($this.position().top);
 
+								$this.css('background-position', 'center ' + (pos * -0.15) + 'px');
 
-//    featured slider
-    $('.featured_slider').slick({
-        centerMode: true,
-        dote: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        speed: 1500,
-        index: 2,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+							});
 
+					};
 
+					off = function() {
 
-//---------------------------------------------
-// Counter 
-//---------------------------------------------
+						$this
+							.css('background-position', '');
 
-    $('.statistic-counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
+						$window
+							.off('scroll._parallax');
 
-//---------------------------------------------
-// Scroll Up 
-//---------------------------------------------
+					};
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 600) {
-            $('.scrollup').fadeIn('slow');
-        } else {
-            $('.scrollup').fadeOut('slow');
-        }
-    });
-    $('.scrollup').click(function () {
-        $("html, body").animate({scrollTop: 0}, 1000);
-        return false;
-    });
+					breakpoints.on('<=medium', off);
+					breakpoints.on('>medium', on);
 
+				});
 
+				return $(this);
 
+			};
 
+			$window
+				.on('load resize', function() {
+					$window.trigger('scroll');
+				});
 
-//About us accordion 
+		}
 
-    $("#faq_main_content").collapse({
-        accordion: true,
-        open: function () {
-            this.addClass("open");
-            this.css({height: this.children().outerHeight()});
-        },
-        close: function () {
-            this.css({height: "0px"});
-            this.removeClass("open");
-        }
-    });
+	// Spotlights.
+		var $spotlights = $('.spotlight');
 
+		$spotlights
+			._parallax()
+			.each(function() {
 
+				var $this = $(this),
+					on, off;
 
+				on = function() {
 
+					var top, bottom, mode;
 
-//Team Skillbar active js
+					// Use main <img>'s src as this spotlight's background.
+						$this.css('background-image', 'url("' + $this.find('.image.main > img').attr('src') + '")');
 
-    jQuery('.teamskillbar').each(function () {
-        jQuery(this).find('.teamskillbar-bar').animate({
-            width: jQuery(this).attr('data-percent')
-        }, 6000);
-    });
+					// Side-specific scrollex tweaks.
+						if ($this.hasClass('top')) {
 
+							mode = 'top';
+							top = '-20%';
+							bottom = 0;
 
+						}
+						else if ($this.hasClass('bottom')) {
 
-//Span Menu toggle
-	$(".navbar-header button").on('click', function(){
-		$('.navbar-header button').toggleClass('active');
-	});
+							mode = 'bottom-only';
+							top = 0;
+							bottom = '20%';
 
-  
+						}
+						else {
 
-    //End
+							mode = 'middle';
+							top = 0;
+							bottom = 0;
 
-});
+						}
 
+					// Add scrollex.
+						$this.scrollex({
+							mode:		mode,
+							top:		top,
+							bottom:		bottom,
+							initialize:	function(t) { $this.addClass('inactive'); },
+							terminate:	function(t) { $this.removeClass('inactive'); },
+							enter:		function(t) { $this.removeClass('inactive'); },
 
+							// Uncomment the line below to "rewind" when this spotlight scrolls out of view.
 
+							//leave:	function(t) { $this.addClass('inactive'); },
 
+						});
 
+				};
 
+				off = function() {
 
+					// Clear spotlight's background.
+						$this.css('background-image', '');
 
+					// Remove scrollex.
+						$this.unscrollex();
 
+				};
 
+				breakpoints.on('<=medium', off);
+				breakpoints.on('>medium', on);
 
+			});
 
+	// Wrappers.
+		var $wrappers = $('.wrapper');
 
+		$wrappers
+			.each(function() {
 
+				var $this = $(this),
+					on, off;
+
+				on = function() {
+
+					$this.scrollex({
+						top:		250,
+						bottom:		0,
+						initialize:	function(t) { $this.addClass('inactive'); },
+						terminate:	function(t) { $this.removeClass('inactive'); },
+						enter:		function(t) { $this.removeClass('inactive'); },
+
+						// Uncomment the line below to "rewind" when this wrapper scrolls out of view.
+
+						//leave:	function(t) { $this.addClass('inactive'); },
+
+					});
+
+				};
+
+				off = function() {
+					$this.unscrollex();
+				};
+
+				breakpoints.on('<=medium', off);
+				breakpoints.on('>medium', on);
+
+			});
+
+	// Banner.
+		var $banner = $('#banner');
+
+		$banner
+			._parallax();
+
+})(jQuery);
